bc86c3e893beb6237f59188dfb886245084de50a
Alfredo Gunn <98775340+96gunna@users.noreply.github.com>
2023-11-27 14:49:45 -0800

    spacing and trying commit

--- FinalProject.py
+++ FinalProject.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template, flash, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from PIL import Image
 import random
-from image_info import image_info
 
 app = Flask(__name__)
 Bootstrap5(app)
 
 app.config['SECRET_KEY'] = 'lilboi'
 
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
-   
     return render_template('index.html')
