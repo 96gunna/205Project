75d6d24adedfd94866fa33f2ca5a85c3d10c359f
Ryan <rmauvais@csumb.edu>
2023-12-04 14:20:41 -0800

    upload fixed for real

--- FinalProject.py
+++ FinalProject.py
@@ -3,28 +3,37 @@ from flask_bootstrap import Bootstrap5
 from PIL import Image
 from io import BytesIO
 import random
-import base64
+import os
+from flask import send_from_directory
+
+UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
 Bootstrap5(app)
 
 app.config['SECRET_KEY'] = 'lilboi'
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('index.html')
 
-
 @app.route('/upload', methods=['POST'])
 def user_upload():
     file = request.files['file']
-    image_data = file.read();
-    image = Image.open(BytesIO(image_data))
-    image_data_base64 = base64.b64encode(image_data).decode('utf-8')
-    # image.show()  # Test to show that we get the image
-    return render_template('upload.html', image_data_base64=image_data_base64)
 
+    if file:
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return render_template('upload.html', image_path=filename)
+    
+    flash('No file uploaded.')
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def serve_uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/examples', methods=('GET', 'POST'))
 def show_examples():
