60dcb94032d2833913aa74dd760d179790f23516
mari-924 <marianaduran260@gmail.com>
2023-12-04 17:55:50 -0800

    changed where we get the image from

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 def Grayscale(p):
-    image = "hw3_images/" + p + ".jpg"
+    image = "uploads/" + p + ".jpg"
     im = Image.open(image)
     g_list = g_list = [(
         int((p[0] * 299 + p[1] * 587 + p[2] * 114) // 1000),
@@ -24,26 +24,26 @@ def sepia(p):
    return r, g, b
 
 def Sepia(p):
-    image = "hw3_images/" + p + ".jpg"
+    image = "uploads/" + p + ".jpg"
     im = Image.open(image)
     s_list = [sepia(pixel) for pixel in im.getdata()]
     im.putdata(s_list)
     im.show()
     
 def Negative(p):
-    image = "hw3_images/" + p + ".jpg"
+    image = "uploads/" + p + ".jpg"
     im = Image.open(image)
     n_list = [(255-p[0], 255-p[1], 255-p[2]) for p in im.getdata()]
     im.putdata(n_list)
     im.show()
 
 def Thumbnail(p):
-    image = "hw3_images/" + p + ".jpg"
+    image = "uploads/" + p + ".jpg"
     im = Image.open(image)
     trgt = im.resize((252,253))
     trgt.show()
 
 def None_(p):
-    image = "hw3_images/" + p + ".jpg"
+    image = "uploads/" + p + ".jpg"
     im = Image.open(image)
     im.show()
