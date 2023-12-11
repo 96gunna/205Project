5153fc8da1731371a42c7383563b1d038d360ad3
Alfredo Gunn <98775340+96gunna@users.noreply.github.com>
2023-12-11 12:55:33 -0800

    transformation image displaying

--- FinalProject.py
+++ FinalProject.py
@@ -16,9 +16,11 @@ Bootstrap5(app)
 app.config['SECRET_KEY'] = 'lilboi'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def user_upload():
@@ -29,28 +31,32 @@ def user_upload():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         return render_template('upload.html', image_path=filename)
-    
+
     flash('No file uploaded.')
     return redirect(url_for('index'))
+
 
 @app.route('/uploads/<filename>')
 def serve_uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+
 @app.route('/examples', methods=('GET', 'POST'))
 def show_examples():
     shuffled_ids = example_ids
     random.shuffle(shuffled_ids)
-    random_example = shuffled_ids[random.randint(0, len(shuffled_ids)-1)]
+    random_example = shuffled_ids[random.randint(0, len(shuffled_ids) - 1)]
     return render_template('examples.html',
                            random_example=random_example)
+
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
     if request.method == 'POST':
         filter_type = request.form.get('filter')
         additional_filter_type = request.form.get('additional_filter')
-        uploaded_image_path = "/uploads/abac-rule-summary.png"  # Replace with the actual path
+        uploaded_image_path = os.path.join(app.config['UPLOAD_FOLDER'], "abac-rule-summary.png")
+        # "/uploads/abac-rule-summary.png"  # Replace with the actual path
 
         if additional_filter_type == 'scaling_fewer_pixels':
             transformed_image_path = scaling_fewer_pixels(uploaded_image_path)
@@ -62,6 +68,7 @@ def edit():
         return render_template('edit.html', transformed_image_path=transformed_image_path)
 
     return render_template('edit.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
