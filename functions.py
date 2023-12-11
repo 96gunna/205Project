dfca4a6143bc0f4478c89558948d1fa79c1bb0a2
mari-924 <marianaduran260@gmail.com>
2023-12-11 12:53:23 -0800

    image edited with filters should be able to display now

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
 from PIL import Image
+import os
 
 def Grayscale(p):
-    image = "uploads/" + p + ".jpg"
+    image = p
     im = Image.open(image)
     g_list = g_list = [(
         int((p[0] * 299 + p[1] * 587 + p[2] * 114) // 1000),
@@ -9,7 +10,11 @@ def Grayscale(p):
         int((p[0] * 299 + p[1] * 587 + p[2] * 114) // 1000)
         ) for p in im.getdata()]
     im.putdata(g_list)
-    im.show()
+
+    output_path = os.path.join("static", "grayscaleEdit.jpg")
+    im.save(output_path)
+    return "grayscaleEdit.jpg"
+   
 
 
 def sepia(p):
@@ -24,26 +29,28 @@ def sepia(p):
    return r, g, b
 
 def Sepia(p):
-    image = "uploads/" + p + ".jpg"
+    image = p
     im = Image.open(image)
     s_list = [sepia(pixel) for pixel in im.getdata()]
     im.putdata(s_list)
-    im.show()
+    output_path = os.path.join("static", "sepiaEdit.jpg")
+    im.save(output_path)
+    return  "sepiaEdit.jpg"
     
 def Negative(p):
-    image = "uploads/" + p + ".jpg"
+    image = p
     im = Image.open(image)
     n_list = [(255-p[0], 255-p[1], 255-p[2]) for p in im.getdata()]
     im.putdata(n_list)
-    im.show()
+    output_path = os.path.join("static", "NegativeEdit.jpg")
+    im.save(output_path)
+    return "NegativeEdit.jpg"
 
 def Thumbnail(p):
-    image = "uploads/" + p + ".jpg"
+    image = p
     im = Image.open(image)
-    trgt = im.resize((252,253))
-    trgt.show()
+    im.resize((252,253))
+    output_path = os.path.join("static", "thumbnailEdit.jpg")
+    im.save(output_path)
+    return "thumbnailEdit.jpg"
 
-def None_(p):
-    image = "uploads/" + p + ".jpg"
-    im = Image.open(image)
-    im.show()
